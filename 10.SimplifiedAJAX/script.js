9ccab9e2a2bdaf53f69345fef45cb0c8dd1d6209
annemessenger <anne_messenger@earthlink.net>
2014-06-10 00:30:54 -0600

    Added Comment
    "Original" styles.css actually has a change (removed height so sample
    code would have gray background throughout)

--- 10.SimplifiedAJAX/script.js
+++ 10.SimplifiedAJAX/script.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
     // Write a simple ajax request here. Investigate using the shorthand methods found at:
     // http://api.jquery.com/category/ajax/shorthand-methods/
 
+    // worked as is, but I removed height from styles.css so sample code fits
+
     $(".target").load("sample.html");
 
 });
