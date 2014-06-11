5676efb23381b355d78be88bbb6b22da9cafeca1
annemessenger <anne_messenger@earthlink.net>
2014-06-10 23:05:25 -0600

    Exercise

--- 3.DOM-Traversal/script.js
+++ 3.DOM-Traversal/script.js
@@ -1,1 +1,7 @@
+$(document).ready(function() {
+
+  // Class exercise: other files were supplied
  
+  $('div.sample > p > span.nextText').text($('div.sample > p > span.targetText').text());
+  $('.targetText').parent().siblings().text($('.targetText').text());
+});
