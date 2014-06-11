d0f8470c3e9b44991cdbe762067345af034451d5
annemessenger <anne_messenger@earthlink.net>
2014-06-10 23:43:10 -0600

    Exercise

--- 8.Disable-Enable/script.js
+++ 8.Disable-Enable/script.js
@@ -1,1 +1,26 @@
- 
+$(document).ready(function() {
+
+  // Class exercise: other files were supplied
+
+  // Disable first button
+  $('#firstButton').prop("disabled", true); 
+
+  // Set callback to demonstrate that first button is really disabled
+  $('#firstButton').click(function() {
+    alert("The first button was clicked.");
+  });
+
+  // Second button's callback
+  $("#secondButton").click(function() {
+
+    // If first button is disabled, enable it; if it's enabled, disable it.
+    var value = $('#firstButton').prop("disabled");
+    if (value === true) {
+      $('#firstButton').prop("disabled", false);
+    }
+    else {
+      $('#firstButton').prop("disabled", true);
+    }
+  }); 
+
+});
