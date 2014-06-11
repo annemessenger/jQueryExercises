2f83d14d4d9ffbe2c181355bc137ed9103783cc8
annemessenger <anne_messenger@earthlink.net>
2014-06-10 23:14:10 -0600

    Exercise

--- 6.Iterating/script.js
+++ 6.Iterating/script.js
@@ -1,1 +1,8 @@
- 
+$(document).ready(function() {
+
+  // Class exercise: other files were supplied
+
+  $('div.sandbox > div > ul > li:even').css('background-color', 'black');
+  $('div.sandbox > div > ul > li:odd').css('background-color', 'gray');
+  $('<p>Note that in jQuery, "even" is index 0, which is list item 1.</p>').appendTo('div.sandbox > div > ul');
+});
