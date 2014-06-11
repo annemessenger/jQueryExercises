be1804d9337fe0a4b05d96a057d2138fb57cdd5d
annemessenger <anne_messenger@earthlink.net>
2014-06-10 22:58:28 -0600

    Exercise

--- 2.DOM-Manipulation/script.js
+++ 2.DOM-Manipulation/script.js
@@ -1,2 +1,9 @@
- 
- 
+$(document).ready(function() {
+
+  // Class exercise: other files were supplied 
+
+  $('.sandbox p').css("color", "red");
+  $('.sandbox p').css("background-color", "yellow");
+  $('.sandbox p').css({"border-style":"solid", "border-color":"blue"});
+  $('.sandbox li').text('Something else.');
+});
